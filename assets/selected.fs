a7214ef682182caefc571587d0a2d670b0f26029
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2012-11-21 21:45:04 +0100

    Add missing shaders

---
+++ assets/selected.fs
@@ -0,0 +1,21 @@
+#ifdef GL_ES
+precision lowp float;
+#endif
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+uniform vec4 vColor;
+
+varying vec2 uvVarying;
+
+void main()
+{
+    vec3 rgb = texture2D(tex0, uvVarying).rgb * vColor.rgb;
+    gl_FragColor = vec4(rgb, texture2D(tex1, uvVarying).a * vColor.a);
+    if (gl_FragColor.a == 0.0) {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 0.1);
+    }
+    else if (gl_FragColor.a <= 0.2) {
+        gl_FragColor = vec4(1.0, .0, .0, 1.0);
+    }
+}
+
