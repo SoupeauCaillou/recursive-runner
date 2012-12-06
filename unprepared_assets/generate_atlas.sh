af8fe5bb6cbf3b78bf3c1e50317f546147eddee6
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2012-12-06 23:45:50 +0100

    clear the script (indications)

--- unprepared_assets/generate_atlas.sh
+++ unprepared_assets/generate_atlas.sh
@@ -1,10 +1,24 @@
+#!/bin/sh
+
+# path need : 
+# location of texture_packer (your_build_dir/sac/build/cmake)
+# location of PVRTexToolCL (need to install it from http://www.imgtec.com/powervr/insider/powervr-pvrtextool.asp)
+# location of etc1tool (android-sdk-linux/tools)
+
+
+if [ $# != 1 ]; then
+	echo "need the image directory"
+	exit
+fi
+
 cd $1
 texture_packer *png | ../../sac/tools/texture_packer/texture_packer.sh $1
 cd -
 convert /tmp/$1.png -alpha extract -depth 8 ../assets/$1_alpha.png
 # mais pourquoi j'ai fait ca ?convert ../assets/$1_alpha.png -background white -flatten +matte -depth 8 ../assets/$1_alpha.png
 convert /tmp/$1.png -background white -alpha off -type TrueColor PNG24:../assets/$1.png
-../../PVRTexTool/PVRTexToolCL/Linux_x86_64/PVRTexTool -f OGLPVRTC4 -yflip0 -i ../assets/$1.png -p -pvrlegacy -m -o ../assets/$1.pvr
+
+PVRTexToolCL -f OGLPVRTC4 -yflip0 -i ../assets/$1.png -p -pvrlegacy -m -o ../assets/$1.pvr
 cp -v /tmp/$1.desc ../assets/
-../../android-sdk-linux/tools/etc1tool --encode ../assets/$1.png -o ../assets/$1.pkm
+etc1tool --encode ../assets/$1.png -o ../assets/$1.pkm
 
